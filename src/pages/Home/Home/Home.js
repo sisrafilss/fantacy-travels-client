06dee8f6a7029ef0f73c28b754af445263fc87b3
Israfil Hossen <sisrafilss@gmail.com>
2022-01-27 07:33:49 +0600

    first commit

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-center display-3 text-success mt-5">
+        Welcome to Fantacy Travels
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
