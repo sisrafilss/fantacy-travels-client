2a772c264e3f939ead600f19837356fa36bdaaaa
Israfil Hossen <sisrafilss@gmail.com>
2022-01-27 09:48:20 +0600

    banner added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navigation from "../../Shared/Navigation/Navigation";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
-      <h2 className="text-center display-3 text-success mt-5">
-        Welcome to Fantacy Travels
-      </h2>
+      <Banner />
     </div>
   );
 };
