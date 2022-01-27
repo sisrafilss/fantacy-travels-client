5b44908176c0cef3f822df3c2b3021aa3eed751c
Israfil Hossen <sisrafilss@gmail.com>
2022-01-27 10:07:01 +0600

    array added for helpoful links in footer

--- src/pages/Shared/Footer/HelpfulLInks/HelpfulLinks.js
+++ src/pages/Shared/Footer/HelpfulLInks/HelpfulLinks.js
@@ -3,32 +3,43 @@ import "./HelpfulLinks.css";
 
 const homePageUrl = "https://fantacy-travels.netlify.app/";
 
+const helpfulLinks = [
+  {
+    name: "Helpful Link 1",
+    url: homePageUrl,
+  },
+  {
+    name: "Helpful Link 2",
+    url: homePageUrl,
+  },
+  {
+    name: "Helpful Link 3",
+    url: homePageUrl,
+  },
+  {
+    name: "Helpful Link 4",
+    url: homePageUrl,
+  },
+  {
+    name: "Helpful Link 5",
+    url: homePageUrl,
+  },
+  {
+    name: "Helpful Link 6",
+    url: homePageUrl,
+  },
+];
+
 const HelpfulLinks = () => {
   return (
     <div className="mt-4">
       <h4 className="pb-3">Helpful Links</h4>
       <div className="helpful-links">
-        <li className="mb-1">
-          <a href={homePageUrl}>Cycling vs Running</a>
-        </li>
-        <li className="mb-1">
-          <a href={homePageUrl}>Importance of Cycling</a>
-        </li>
-        <li className="mb-1">
-          <a href={homePageUrl}>How Cycling impact your Fitness</a>
-        </li>
-        <li className="mb-1">
-          <a href={homePageUrl}>Cycling vs Running</a>
-        </li>
-        <li className="mb-1">
-          <a href={homePageUrl}>Importance of Cycling</a>
-        </li>
-        <li className="mb-1">
-          <a href={homePageUrl}>How Cycling impact your Fitness</a>
-        </li>{" "}
-        <li className="mb-1">
-          <a href={homePageUrl}>Cycling vs Running</a>
-        </li>
+        {helpfulLinks.map((link, index) => (
+          <li key={index} className="mb-1">
+            <a href={link.url}>{link.name}</a>
+          </li>
+        ))}
       </div>
     </div>
   );
